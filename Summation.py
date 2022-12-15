4d7f3c6352384abc5e229315456216537e361ef1
kimalfred <119164038+kimalfred@users.noreply.github.com>
2022-12-15 12:32:40 +0800

    Create Summation.py

---
+++ Summation.py
@@ -0,0 +1,16 @@
+num1 = input('Enter first number: ') # string input
+
+#System.out.println("Enter first number");
+#float num1 = input.nextFloat();
+
+num2 = input("Enter second number: ")
+
+# Add two numbers
+sum = float(num1) + float(num2) # conversion takes place from String to number
+
+# Display the sum
+print('\nThe sum of {0} and {1} is {2}'.format(num1, num2, sum)) # format to concatenate 
+
+#print('\nThe sum of {0} and {1} is {2}',(num1, num2, sum))
+
+print("\nThe sum is "+(num1 + num2)) # String output
